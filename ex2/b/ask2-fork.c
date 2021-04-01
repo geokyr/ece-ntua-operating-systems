3445ea479589993389c65d037e66de831d707554
SerTze <sertze2000@gmail.com>
2021-04-01 21:02:30 +0300

    final ex2.b

--- ex2/b/ask2-fork.c
+++ ex2/b/ask2-fork.c
@@ -90,18 +90,12 @@ int main(int argc, char *argv[])
 }
 
 
-/* 1. With sleep(1) included, initialization messages are printed by breadth, meaning that all processes 
-* of current level (A) print the initialization message before the ones in the next level (B, C, D) and 
-* the final initialization messages are the ones of the final level (E, F). 
+/* 1. Initialization messages are printed by breadth, meaning that all processes of current level (A) 
+* print the initialization message before the ones in the next level (B, C, D) and the final initialization
+* messages are the ones of the final level (E, F). 
 * 
 * Exit messages have a similar principle. More specifically, the first exit messages are printed from the 
 * lowest level leaf processes (C, D), then the leafs of the higher level (E, F). Afterwards, B, the 
 * father of E and F, exits through the wait_for_ready_children() function. Finally, A, whose all children
 * processes are terminated, exits in a similar way.
-*
-* If sleep(1) is not included, the forks and kills(exits) still execute in a similar order, however, because
-* of the fact that these calls are really close to each other, and that functions like forker are run in a
-* parallel manner, the exact "breadth first" order is not followed, oppossed to above. Some messages can be
-* swapped, as a forker function of a deeper level makes a fork earlier than the forking of the 2nd child in
-* the previous level, etc.
 */
