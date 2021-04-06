4b6d5e64ca9bbb8c720a910e2639e6883807c5ee
geokyr <imgeorgekyr@gmail.com>
2021-04-06 13:28:54 +0300

    final ex2.a

--- ex2/a/ask2-fork.c
+++ ex2/a/ask2-fork.c
@@ -15,20 +15,21 @@
  * A-+-B---D
  *   `-C
  */
+
 void fork_procs(void)
 {
 	/*
 	 * initial process is A.
 	 */
+
 	printf("A: Initiating...\n");
 	change_pname("A");
 
 	/* ... */
-
 	pid_t pidB, pidC, pidD;
 	int status;
 
-	//child B
+	/* Child B */
 	pidB = fork();
 
 	if(pidB < 0) {
@@ -39,7 +40,7 @@ void fork_procs(void)
 		printf("B: Initiating...\n");
 		change_pname("B");
 
-		//child D
+		/* Child D */
 		pidD = fork();
 
 		if(pidD < 0) {
@@ -51,18 +52,16 @@ void fork_procs(void)
 			change_pname("D");
 			printf("D: Sleeping...\n");
 			sleep(SLEEP_PROC_SEC);
-			printf("D: Exiting...\n");
 			exit(13);
 		}
 
 		printf("B: Waiting...\n");
 		pidD = wait(&status);
 		explain_wait_status(pidD, status);
-		printf("B: Exiting...\n");
 		exit(19);
 	}
 
-	//child C
+	/* Child C */
 	pidC = fork();
 
 	if(pidC < 0) {
@@ -74,7 +73,6 @@ void fork_procs(void)
 		change_pname("C");
 		printf("C: Sleeping...\n");
 		sleep(SLEEP_PROC_SEC);
-		printf("C: Exiting...\n");
 		exit(17);
 	}
 
@@ -86,8 +84,6 @@ void fork_procs(void)
 	explain_wait_status(pidB, status);
 
 	/* ... */
-
-	printf("A: Exiting...\n");
 	exit(16);
 }
 
@@ -103,6 +99,7 @@ void fork_procs(void)
  *      use wait_for_ready_children() to wait until
  *      the first process raises SIGSTOP.
  */
+
 int main(void)
 {
 	pid_t pid;
@@ -115,7 +112,7 @@ int main(void)
 		exit(1);
 	}
 	if (pid == 0) {
-		/* Child */
+		/* Child A */
 		fork_procs();
 		exit(1);
 	}
@@ -131,16 +128,13 @@ int main(void)
 
 	/* Print the process tree root at pid */
 	
-	show_pstree(pid);
+	show_pstree(getpid());
 	/* for ask2-signals */
 	/* kill(pid, SIGCONT); */
 
 	/* Wait for the root of the process tree to terminate */
 	pid = wait(&status);
 	explain_wait_status(pid, status);
-
-
-
 
 	return 0;
 }
@@ -149,18 +143,19 @@ int main(void)
 /* 1. Exit message "terminated by a signal" then B, C, D exit as well but only
  * D produces exit message "terminated normally", since orphaned processes B, C 
  * are adopted by init (PID = 1) which performs the waiting system until the orphaned
- * processes terminate. Since return 0 is run after A being killed, before B, C, D 
- * exit, the program exits the prompt and exit messages for B, C, D are written on the
- * next line leaving the prompt running. One solution to this could be adding a 
- * sleep(SLEEP_PROC_SEC) before the return 0 command, so that exit messages for 
- * B, C, D are printed on the prompt before it exits.
+ * processes terminate. We can see that using the ps -ef command after killing process
+ * A and again a few seconds later, when D has exiting normally, and B, C have exited
+ * after being adopted by init, without producing an exit message (since their parent
+ * A was responsible for that).
  * 
  * 2. Processes ask2-fork (root), sh (child) and pstree (grandchild) also appear on the 
  * tree as a second branch with the first branch being the previous one (using pid instead 
  * of getpid()) with processes A, B, C, D. This is because the function system within
  * show_pstree uses fork to create a child process that executes the command created 
- * with sprintf (including pstree). 
+ * with sprintf (including pstree).
  *
- * 3. Running out of memory, prevent fork bombs, not recycling PIDs (wrap) causing loss of 
- * current processes.
+ * 3. If there was no user limit, a user could create, whether intentionally or not, 
+ * a big enough number of processes, therefore having the system run out of memory.
+ * In addition, this user limit prevents fork bombs, which when executed can slow down
+ * or crash the system and require a restart, due to resource starvation.
  */
